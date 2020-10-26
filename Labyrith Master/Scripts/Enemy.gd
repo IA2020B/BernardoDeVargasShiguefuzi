579bdb060e1f4a357ed26407746cb438009cd94c
MeatballSenpai <67079442+MeatballSenpai@users.noreply.github.com>
2020-10-26 17:45:52 -0300

    Mudanças pequenas p/ o código de Enemy

--- Labyrith Master/Scripts/Enemy.gd
+++ Labyrith Master/Scripts/Enemy.gd
@@ -1,19 +1,20 @@
 extends KinematicBody2D
+# Esse é o script dos inimigos. Contem uma máquina de estados finitos
 
-# Enum dos estados
+# Enum dos estados finitos
 enum {
 	IDDLE, 
 	ATTACK, 
-	PERSUE, 
+	CHASE, 
+	DETECTION, 
 	FLEE, 
-	MOVE,
-	NEW_DIRECTION,
-	DETECTION}
+	MOVE, 
+	NEW_DIRECTION}
 
 # Váriaveis 
 const SPEED = 100
 var state = MOVE
-var direction = Vector2.RIGHT
+var direction = Vector2.ZERO
 var target = null
 
 # Função p/ quando o nodo entra em cena
@@ -23,41 +24,39 @@ func _ready():
 
 # Função de processo (por tempo)
 func _process(delta):
-	# Estados
+	# Estados (0)
 	match state:
 		# Parado
 		IDDLE:
 			pass
 		
-		# Ataque o jogador 
 		ATTACK:
+			attack_player()
+		
+		# Perseguir
+		CHASE:
 			pass
 		
-		# Persiga o jogador 
-		PERSUE:
-			persue_player()
+		# Detectar jogador
+		DETECTION:
+			_on_DetectionRange_body_entered(target)
+			state = ATTACK
 		
-		# Fuja do jogador 
+		# Fugir do jogador
 		FLEE:
 			pass
 		
-		# Se mova na direção atual
+		# Se mover em direção atual
 		MOVE:
 			move(delta)
 		
-		# Escolha uma nova direção
+		# Escolher nova direção
 		NEW_DIRECTION:
-			# Escolha uma direção
-			direction = choose([Vector2.RIGHT, Vector2.LEFT, Vector2.UP, Vector2.DOWN])
-			# Escolha um novo estado
+			direction = choose([Vector2.UP, Vector2.DOWN, Vector2.RIGHT, Vector2.LEFT])
 			state = choose([IDDLE, MOVE])
-		
-		# Detecte o jogador
-		DETECTION:
-			_on_DetectionRange_body_entered(target)
 
 # Função de movimento
-func move(delta):
+func move (delta):
 	position += direction * SPEED * delta
 	move_and_collide(direction)
 
@@ -73,13 +72,15 @@ func _on_Timer_timeout():
 	$Timer.wait_time = choose([0.5, 1, 1.5])
 	state = choose([IDDLE, MOVE, NEW_DIRECTION])
 
-# Função de detecção de jogador (sinal)
+# Função para detectar quando o jogador entra no circulo de deteção
 func _on_DetectionRange_body_entered(body):
-	# Se o nome do objeto que entrou
 	if body.name == "Player":
 		target = body
-		state = PERSUE
 
-# Função p/ perseguir o jogador
-func persue_player():
-	print ("Get Him!")
+# Função para atacar o jogador
+func attack_player():
+	pass
+
+# Função para quando o jogador sair do campo de detecção
+func _on_DetectionRange_body_exited(body):
+	pass
